28e59dd230ca36378eea68df8efc605b4efbd92c
Adam khairi <khairiadam2@gmail.com>
2020-03-13 16:26:43 +0100

    Complete Logic(JS)

--- src/js/general.js
+++ src/js/general.js
@@ -9,6 +9,7 @@ function popUp() {
 
 const tools = document.getElementsByClassName("icon");
 const section = document.getElementById("section");
+
 const imgs = [
     "./src/img/icon-paper.svg",
     "./src/img/icon-scissors.svg",
@@ -17,7 +18,11 @@ const imgs = [
 const classes = ["paper", "scissors", "rock"];
 const user = document.querySelector(".user");
 const cpu = document.querySelector(".cpu");
+const rst = document.getElementById('rst');
+
 let score = document.getElementById("score-global");
+let result = document.getElementById('result');
+
 
 console.log(tools);
 ///////////////////////
@@ -44,39 +49,51 @@ for (let i = 0; i < 3; i++) {
 
         let userChoice = i;
         winner(userChoice, cpuChoise);
-
+        rst.addEventListener('click', function () {
+            reset(child1, child2, userChoice, cpuChoise);
+        })
     });
 }
 ////////////// Random Choise of CPU /////////////
 function cpuChois() {
     return Math.floor(Math.random() * 3);
 }
-
-let globScore = Number(score.textContent);
 function winner(a, b) {
-    let result = document.getElementById('result');
+    ////////// Show Reset_BTN ///////
+    rst.style.display = 'block';
+    let globScore = Number(score.textContent);
+
     if ((a == 2 && b == 1) || (a == 1 && b == 0) || (a == 0 && b == 2)) {
-        ////////// Incrementing User Score ////////
 
         result.textContent = "You Win";
-        score.textContent = globScore + 1;
+        ////////// Incrementing User Score ////////
+        globScore++;
+        score.textContent = globScore
 
     } else if (a == b) {
         choices = document.getElementById("choices");
         result.textContent = "It's a draw";
 
     } else {
+        globScore = globScore - 1;
+        score.textContent = globScore
 
-        score.textContent = globScore - 1;
         result.textContent = "You Lose...!";
-
     }
 }
-function reset(){
-    const rst = document.getElementById('rst');
+function reset(child1, child2, userChoice, cpuChoise) {
+    rst.style.display = 'none';
+    section.style.display = "block";
 
+    user.removeChild(child1);
+    user.classList.remove("icon");
+    user.classList.remove(classes[userChoice]);
+
+    cpu.removeChild(child2);
+    cpu.classList.remove("icon");
+    cpu.classList.remove(classes[cpuChoise]);
+    result.textContent = ""
 }
-
 
 // function winner(a, b) {
 //     if (((a == 0) && (b == 1 || b == 3)) || ((a == 1) && (b == 2 || b == 4)) || ((a == 2) && (b == 0 || b == 3)) || ((a == 3) && (b == 1 || b == 4)) || ((a == 4) && (b == 2 || b == 0))) {
